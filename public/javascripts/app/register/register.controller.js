a8ab6c1b83bebceb795ca8b3162e18c6d662fd61
Eric Cao <eric.cao@nanocloud.com>
2016-05-01 19:19:10 +0200

    user can create an account and login

--- public/javascripts/app/register/register.controller.js
+++ public/javascripts/app/register/register.controller.js
@@ -1,20 +1,25 @@
 angular.module('application')
   .controller('RegisterController', controller);
 
-function controller() {
+controller.$inject = ['$http'];
+
+function controller($http) {
 
   this.user = {
     username: "",
+    email: "",
     password: "",
     confirmation: "",
   }
 
   this.createAccount = function() {
     if (this.user.username != "" &&
+      this.user.email != "" &&
       this.user.password != "" &&
       this.user.confirmation != "") {
 
       if (this.user.password == this.user.confirmation) {
+        $http.post("/api/users/create", { username: "spacewoox", password: "admin", mail: "spacewoox@gmail.com" });
         return alert('ok');
       }
     }
